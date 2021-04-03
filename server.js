2c1ef5626e604a2c8e952cf32c9b61e98d56faeb
Rahul Raina <rahulraina711@gmail.com>
2021-04-03 13:46:47 +0530

    added static folder

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const http = require('http');
 
+// Creating an express server to be used for Socket.io as well
+const app = express();
+const server = http.createServer(app);
+
+// Setting up Port
 const PORT = 3100 || process.env.PORT;
 
 // Setting up static floder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Listening to port
-app.listen(PORT, ()=>{
+server.listen(PORT, ()=>{
     console.log(`Server up and running on port : ${PORT}`);
 })
 
