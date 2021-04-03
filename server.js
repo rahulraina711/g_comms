c3ac75167f4def193265e4fd5eab2bd8da6bdd7b
Rahul Raina <rahulraina711@gmail.com>
2021-04-03 13:51:40 +0530

    socket connection has been successfully set up

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const socketio = require('socket.io');
 
 // Creating an express server to be used for Socket.io as well
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 // Setting up Port
 const PORT = 3100 || process.env.PORT;
 
 // Setting up static floder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// run when a client connects
+io.on('connection', socket=>{
+    console.log('new socket connection...');
+})
 
 // Listening to port
 server.listen(PORT, ()=>{
